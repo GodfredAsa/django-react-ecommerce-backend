4fd95826a1c87ce1688eac89b20195388856d0e0
GodfredAsa <goldenheroe1@yahoo.com>
2022-12-26 12:03:06 +0000

    order view and order url implementation

--- base/urls/product_urls.py
+++ base/urls/product_urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from base.views import product_views
 
 urlpatterns = [
-    path('/', product_views.getProducts, name='products'),
-    path('/<str:pk>/', product_views.getProduct, name='product'),
+    path('', product_views.getProducts, name='products'),
+    path('/<str:pk>', product_views.getProduct, name='product'),
 ]
