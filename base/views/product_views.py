a8b25f4c98246520c5a83aa1f74c1261c255e37b
GodfredAsa <goldenheroe1@yahoo.com>
2023-02-03 23:35:16 +0000

    implementing product reviews

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,9 +1,9 @@
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAdminUser, IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
-from base.models import Product
+from base.models import Product, Review
 from base.serializers import ProductSerializer
 
 @api_view(['GET'])
@@ -58,6 +58,7 @@ def deleteProduct(request, pk):
     return Response({'details': 'Product successfully deleted'}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
+@permission_classes([IsAdminUser])
 def uploadProductImage(request):
     data = request.data 
     product_id = data['product_id']
@@ -65,3 +66,46 @@ def uploadProductImage(request):
     product.image = request.FILES.get('image')
     product.save()
     Response({'details':'Image Uploaded Successfully'}, status=status.HTTP_200_OK)
+    
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def createProductReview(request, pk):
+    user = request.user
+    product = Product.objects.get(_id = pk)
+    data = request.data
+    
+    # SCENARIOS 
+    # 1. review already exists
+    alreadyExists = product.review_set.filter(user = user).exists()
+    if alreadyExists:
+        content = {'details': 'Product already Reviewed'}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+    
+    # 2 No rating or rating is 0
+    elif data['rating'] == 0:
+        content = {'details': 'Please Select a Rating '}
+        return Response(content, status=status.HTTP_400_BAD_REQUEST)
+        
+    # 3 Create review 
+    else:
+        review = Review.objects.create(
+            user = user,
+            product = product,
+            name = user.first_name,
+            rating = data['rating'],
+            comment = data['comment']
+        )
+        
+        # we need to update the review of the product of the model
+        reviews = product.review_set.all()
+        product.numReviews = len(reviews)
+         
+        total = 0
+        for i in reviews:
+            total += i.rating
+        # calculate rating 
+        product.rating = total / len(reviews)
+        # save product with review and rating 
+        product.save()
+        return Response('Review Added Successfully')
+    
